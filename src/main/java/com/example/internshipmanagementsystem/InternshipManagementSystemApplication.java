17b137bcc5b2375e2f6f07eda6345b1833f1f5f9
KevinWang12138 <wangqianming12138@gmail.com>
2023-02-27 17:26:51 +0800

    创建空项目

---
+++ src/main/java/com/example/internshipmanagementsystem/InternshipManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.internshipmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternshipManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternshipManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/internshipmanagementsystem/InternshipManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internshipmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
