d81c3a43871e1e8d8e345ba71d3429575f912bf6
KevinWang12138 <wangqianming12138@gmail.com>
2023-03-03 16:46:16 +0800

    搭建日志系统，支持控制台sql日志，支持INFO和ERR日志打印在文件中，支持打印请求日志

---
+++ src/main/java/com/wqm/ims/aop/RequestAdvice.java
@@ -0,0 +1,39 @@
+package com.wqm.ims.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.HttpInputMessage;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.RequestBodyAdvice;
+
+import java.lang.reflect.Type;
+
+@Slf4j
+@RestControllerAdvice
+public class RequestAdvice implements RequestBodyAdvice {
+    @Override
+    public boolean supports(MethodParameter methodParameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return true;
+    }
+
+    @Override
+    public HttpInputMessage beforeBodyRead(HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return inputMessage;
+    }
+
+    @Override
+    public Object afterBodyRead(Object body, HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+        log.info("--request start--");
+        log.info("调用方法：{}", parameter.getMethod().getName());
+        log.info("请求报文：{}", body);
+        log.info("--request end--");
+        return body;
+    }
+
+    @Override
+    public Object handleEmptyBody(Object body, HttpInputMessage inputMessage, MethodParameter parameter, Type targetType, Class<? extends HttpMessageConverter<?>> converterType) {
+        return body;
+    }
+}
+
